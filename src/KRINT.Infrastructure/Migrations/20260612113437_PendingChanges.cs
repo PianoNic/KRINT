127045f56a8600d32e0c82a4cbd56a9821d3df4e
PianoNic <79938743+Pianonic@users.noreply.github.com>
2026-06-12 13:55:20 +0200

    Add migration for MigratedToInstanceId column

---
+++ src/KRINT.Infrastructure/Migrations/20260612113437_PendingChanges.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KRINT.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MigratedToInstanceId",
+                table: "DatabaseInstances",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MigratedToInstanceId",
+                table: "DatabaseInstances");
+        }
+    }
+}
